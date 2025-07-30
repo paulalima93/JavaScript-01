87d39ea9a6007ede77e303c66f21e72fe78e9837
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2025-07-29 21:16:42 -0300

    Create exercicios07.js

---
+++ exercicios07.js
@@ -0,0 +1,37 @@
+// Aula 7 – Arrays e loop for
+// Exercícios de Fixação
+
+// ----------------------------------------
+// 1. Crie um array com 5 cores favoritas e exiba cada uma com a mensagem:
+// "Minha cor favorita é: [cor]"
+// ----------------------------------------
+
+
+
+// ----------------------------------------
+// 2. Crie um array com 6 idades. Mostre apenas as idades maiores ou iguais a 18 anos.
+// ----------------------------------------
+
+
+
+// ----------------------------------------
+// 3. Crie um array com 7 palavras. Conte e exiba quantas têm mais de 5 letras.
+// ----------------------------------------
+
+
+
+// ----------------------------------------
+// 4. Crie um array com 5 valores booleanos. Conte quantos são `true`.
+// ----------------------------------------
+
+
+
+// ----------------------------------------
+// 5. Crie um array com 4 alturas em metros (ex: 1.65). Calcule e exiba a média das alturas.
+// ----------------------------------------
+
+
+
+// ----------------------------------------
+// 6. Crie um array com 5 nomes de animais. Mostre apenas os nomes que começam com a letra "C".
+// ----------------------------------------
